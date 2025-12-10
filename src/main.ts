642a5ccf30c917cc10bea995741b9f20877e274c
fadliraamdhn <fadli16ramadhan@gmail.com>
2025-12-11 03:54:11 +0700

    add allowed header

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,16 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const expressApp = app.getHttpAdapter().getInstance();
+
+  expressApp.set('trust proxy', 1);
   app.use(cookieParser());
   app.enableCors({
     origin: process.env.CORS,
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "Content-Type, Authorization"
+    allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
+    exposedHeaders: ["Set-Cookie"]
   })
 
   await app.listen(process.env.PORT ?? 4000);
